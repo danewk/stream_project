cbf5421541834f7472549132bcc11f5a6df977f3
kdanwoo <kdanwoo@jobis.co>
2022-05-05 13:31:30 +0900

    Initial commit

---
+++ src/com/kdanwoo/Chapter3Section1.java
@@ -0,0 +1,16 @@
+package com.kdanwoo;
+
+import com.kdanwoo.util.Adder;
+import java.util.function.Function;
+
+public class Chapter3Section1 {
+  public static void main(String[] args) {
+    Function<Integer, Integer> myAdder = new Adder();
+    Function<Integer, Integer> myAdder2 = x -> x + 10; // anonymous function
+
+    Integer result = myAdder.apply(5);
+    Integer result2 = myAdder2.apply(5);
+    System.out.println(result);
+    System.out.println(result2);
+  }
+}

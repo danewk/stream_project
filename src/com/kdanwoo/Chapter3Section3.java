c27682a7dfee6fc5df685cee7b8a8b85975e661e
kdanwoo <kdanwoo@jobis.co>
2022-05-05 13:37:42 +0900

    bifunction interface

---
+++ src/com/kdanwoo/Chapter3Section3.java
@@ -0,0 +1,15 @@
+package com.kdanwoo;
+
+import java.util.function.BiFunction;
+
+public class Chapter3Section3 {
+
+  public static void main(String[] args) {
+    BiFunction<Integer, Integer, Integer> adder = (x, y) -> x + y;
+
+    int result = adder.apply(3, 5);
+
+    System.out.println(result);
+
+  }
+}
